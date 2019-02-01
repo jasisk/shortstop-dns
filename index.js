bcc92773d168069679cc0a25fdb53a42f5afcd6a
Jean-Charles Sisk <jasisk@gmail.com>
2019-02-01 17:12:28 -0500

    1.1.0

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const { lookup } = require('dns');
 
+const withPort = port => port !== undefined ? address => address + ':' + port : address => address;
+const PortRE = /:([0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
+
 module.exports = ({ family = 4, all = true }={}) => (address, cb) => {
+  const [, port] = address.match(PortRE) || [];
+  const format = withPort(port);
+
+  address = address.replace(PortRE, '');
+
   lookup(address, { family, all }, (err, ...args) => {
     if (err) { return cb(err); }
     const [addresses, family] = args;
     if (family) {
-      return cb(null, addresses);
+      return cb(null, format(addresses));
     }
-    cb(null, addresses.map(({ address }) => address));
+    cb(null, addresses.map(({ address }) => format(address)));
   });
 };
--- test.js
+++ test.js
@@ -23,6 +23,30 @@ test('good host', t => {
   t.end();
 });
 
+test('good host with port', t => {
+  const port = 1234;
+  t.test('default arguments', t => {
+    const dns = initDns();
+    dns('localhost:' + port, (err, addresses) => {
+      t.error(err);
+      t.type(addresses, 'Array', 'returns an array');
+      t.ok(addresses.length, 'returns at least one address');
+      t.match(addresses, new RegExp('127.0.0.1:' + port), 'contains 127.0.0.1:port');
+      t.end();
+    });
+  });
+  t.test('all: false', t => {
+    const dns = initDns({all: false});
+    dns('localhost:' + port, (err, addresses) => {
+      t.error(err);
+      t.type(addresses, 'string', 'returns a String');
+      t.ok(addresses.endsWith(':' + port), 'end with the port');
+      t.end();
+    });
+  });
+  t.end();
+});
+
 test('bad host', t => {
   const dns = initDns();
   dns('this.will.never.exist.duh', (err, addresses) => {
